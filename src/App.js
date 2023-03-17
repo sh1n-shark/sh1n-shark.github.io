4c42ecc4000ddb967beb3a32ba000b0c89ccc1f4
SH1N.shark <4384752+sh1n-shark@users.noreply.github.com>
2023-03-17 01:49:46 +0000

    Readded google analytics.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 import LinkTree from './components/LinkTree';
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = "G-M9F8FE0BVD";
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
