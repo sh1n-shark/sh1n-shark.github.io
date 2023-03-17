5e53483e85a69c8826c6729b95cd9aad42821a88
SH1N.shark <4384752+sh1n-shark@users.noreply.github.com>
2023-03-17 02:07:54 +0000

    Updating to react-ga4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-M9F8FE0BVD");
 ReactDOM.render(<App />, document.getElementById('root'));
