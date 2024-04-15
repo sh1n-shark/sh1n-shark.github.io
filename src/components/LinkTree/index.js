404dd4a09f8972da3e4aa23c94b155ce9c18a295
SH1N.shark <4384752+sh1n-shark@users.noreply.github.com>
2024-04-15 19:21:29 +0300

    Remove discord and buymeacoffee links

--- src/components/LinkTree/index.js
+++ src/components/LinkTree/index.js
@@ -36,11 +36,9 @@ export default function LinkTree() {
       <Header picture={sh1nLogo} title='SH1N.shark' />
       <Button link='https://twitch.tv/sh1n_shark' icon={twitchLogo} name='Twitch' backgroundColor={variables.twitchColor} />
       <Button link='https://www.tiktok.com/@sh1n.shark' icon={tiktokLogo} name='TikTok' backgroundColor={variables.tiktokColor} />
-      <Button link='https://discord.gg/vQF4z3C9h7' icon={discordLogo} name='Discord Server' backgroundColor={variables.discordColor} />
       <Button link='https://bsky.app/profile/shin.baby' style='border-radius: 50%;' icon={bskyLogo} name='BlueSky' backgroundColor={variables.bskyColor} />
       <Button link='https://twitter.com/sh1n_shark' icon={twitterLogo} name='Twitter' backgroundColor={variables.twitterColor} />
       <Button link='https://vrchat.com/home/user/usr_15edda65-7334-44be-8203-50bd8f7cfd84' icon={vrcLogo} name='VRChat' backgroundColor={variables.vrcColor}  />
-      <Button link='https://www.buymeacoffee.com/sh1nshark' icon={bmcLogo} name='Buy Me A Coffee' backgroundColor={variables.bmcColor}  />
       <Button link='https://sh1nshark.gumroad.com/' icon={gumroadLogo} name='Gumroad' backgroundColor={variables.gumroadColor} />
       <Button link='https://furaffinity.net/user/sh1n.shark' icon={faLogo} name='Furaffinity' backgroundColor={variables.faColor} />
 		</Container>
